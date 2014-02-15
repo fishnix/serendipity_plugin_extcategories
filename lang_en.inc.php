72cc4089449b4e9ab4560c894b8d04964fdf870f
E Camden Fisher <fish@fishnix.net>
2014-02-15 12:53:01 -0500

    initial commit

---
+++ lang_en.inc.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+ *  @version $Revision: 1381 $
+ *  @author E Camden Fisher <fishnix@gmail.com>
+ *  EN-Revision: Revision of lang_en.inc.php
+ */
+
+
+@define('EXTCATEGORIES',	'Extended Categories');
+@define('EXTCATEGORY_PLUGIN_DESC', 'An extended version of the serendipity categories plugin.');
+
+@define('DESC',	'Description');
+@define('DESC_FOR_NUGGET', 'Additional text for nugget, useful if you are using plugin_categories.tpl below.');
+
+?>
